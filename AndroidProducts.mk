4c69fa5f025f003d012a5ab64a783361739f692c
jro1979oliver <jroliveira.oliveira301@gmail.com>
2020-06-04 21:11:11 +0000

    harpia: Havoc

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_harpia.mk
+    $(LOCAL_DIR)/havoc_harpia.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_harpia-userdebug \
-    lineage_harpia-eng
+    havoc_harpia-userdebug \
+    havoc_harpia-eng
--- havoc_harpia.mk
+++ havoc_harpia.mk
@@ -16,15 +16,15 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common havoc stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 # Inherit from harpia device
 $(call inherit-product, device/motorola/harpia/device.mk)
 
 ## Device identifier. This must come after all inclusions
 BUILD_FINGERPRINT := motorola/harpia/harpia:6.0.1/MPI24.241-15.3/3:user/release-keys
 PRODUCT_DEVICE := harpia
-PRODUCT_NAME := lineage_harpia
+PRODUCT_NAME := havoc_harpia
 PRODUCT_MODEL := Moto G Play
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
