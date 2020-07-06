d2ae276f37811e17d55be057fe896f7d4cf16002
jro1979oliver <jroliveira.oliveira301@gmail.com>
2020-07-06 17:39:01 +0000

    harpia: Arrow bringup
    Change-Id: I1679f15174542f0bbed974afe12d17d69ca73f87

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_harpia.mk
+    $(LOCAL_DIR)/arrow_harpia.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_harpia-userdebug
+    arrow_harpia-userdebug
